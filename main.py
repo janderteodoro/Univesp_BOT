59454c2b0d91aa92866e069e39cce213582698d5
Jander Teodoro <79011974+janderteodoro@users.noreply.github.com>
2022-03-21 22:04:14 -0300

    equeleto completo

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from json import load
 from dotenv import dotenv_values
 import discord 
 from discord.ext import commands 
+from time import sleep
+
 
 config = dotenv_values('.env')
 
@@ -9,6 +11,22 @@ bot = commands.Bot('!')
 
 @bot.event
 async def on_ready():
-    print(f'Estou pront! Estou conectado como {bot.user}')
+    print(f'Estou pront!o Estou conectado como {bot.user}')
+
+@bot.event 
+async def on_message(message): 
+    if message.author == bot.user:
+        return 
+
+    await bot.process_commands(message)
+
+
+@bot.command(name='oi')
+async def send_hello(ctx):
+    name = ctx.author.name 
+    response = f'Olá {name}, sou assistente virtual da UNIVESP, qual a sua dúvida?'
+    await ctx.send(response)
+
+
 
 bot.run(config['TOKEN'])
